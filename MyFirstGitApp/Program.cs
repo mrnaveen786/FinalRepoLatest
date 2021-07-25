801a1ac15e6fdbf67b63765b89b9268e2e493eb2
mrnaveen786 <73769251+mrnaveen786@users.noreply.github.com>
2021-07-25 02:30:18 -0400

    Update Program.cs

--- MyFirstGitApp/Program.cs
+++ MyFirstGitApp/Program.cs
@@ -20,7 +20,7 @@ namespace MyFirstGitApp
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                   //webBuilder.UseStartup<Startup>();
                 });
     }
 }
